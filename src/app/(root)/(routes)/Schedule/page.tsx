54ef2cdf57493d5c1dda16efb5a36307d624eafb
Skeeter Cathcart <skeetercathcart@gmail.com>
2023-09-11 19:08:56 -0500

    -hmmm

--- src/app/(root)/(routes)/Schedule/page.tsx
+++ src/app/(root)/(routes)/Schedule/page.tsx
@@ -3,12 +3,7 @@ import CalendarPage from "@/Components/Schedule/ScheduleComponent";
 const Calendar = () => {
     return (
       <div>
-        <div>
         <CalendarPage />
-      </div>
-        <div>
-            <h1>Next Two Weeks</h1>
-        </div>
       </div>
     );
   };
